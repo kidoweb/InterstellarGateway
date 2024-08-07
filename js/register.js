bccbfb37b3f4930780bc15f95496568cc467dcfa
kidoweb <teaserverteams@gmail.com>
2024-08-07 20:33:19 +0300

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,8 +1,7 @@
-// js/register.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, sendEmailVerification } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
 
-// Ваши параметры Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyBo6sm-D7KpWhnYkjZrhJnoIE0sNwpTVIc",
     authDomain: "interstellargateway-149ae.firebaseapp.com",
@@ -14,23 +13,35 @@ const firebaseConfig = {
     measurementId: "G-BEH8RS3YGB"
 };
 
-// Инициализация Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const database = getDatabase(app);
 
-document.addEventListener('DOMContentLoaded', () => {
-    const registerForm = document.getElementById('register-form');
+document.getElementById('register-form').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const username = document.getElementById('username').value;
 
-    registerForm.addEventListener('submit', async (event) => {
-        event.preventDefault();
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
+    try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        const user = userCredential.user;
 
-        try {
-            await createUserWithEmailAndPassword(auth, email, password);
-            window.location.href = 'profile.html'; // Перенаправление на профиль после регистрации
-        } catch (error) {
-            alert('Ошибка регистрации: ' + error.message);
-        }
-    });
+        // Отправка письма с подтверждением почты
+        await sendEmailVerification(user);
+        alert('Письмо с подтверждением отправлено. Пожалуйста, проверьте свою почту.');
+
+        // Добавление данных пользователя в базу данных
+        await set(ref(database, 'users/' + user.uid), {
+            username: username,
+            email: email,
+            group: 'user', // Дефолтная группа пользователя
+            avatar: 'default-avatar.png'
+        });
+
+        // Перенаправление пользователя на страницу профиля после регистрации
+        window.location.href = 'profile.html';
+    } catch (error) {
+        alert('Ошибка регистрации: ' + error.message);
+    }
 });
