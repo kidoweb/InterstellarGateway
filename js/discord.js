b2251dfe68a831ea5b07ef3bfdb8db654b560356
kidoweb <teaserverteams@gmail.com>
2024-08-07 21:15:48 +0300

    Create discord.js

---
+++ js/discord.js
@@ -0,0 +1,6 @@
+// В этом примере нет необходимости в JavaScript коде для функциональности Discord,
+// так как функциональность просто перенаправляет на Discord сервер по нажатию кнопки.
+
+document.getElementById('discord-button').addEventListener('click', function() {
+    window.location.href = 'https://discord.gg/sn28ff4Zwq'; // Перенаправление на сервер Discord
+});
